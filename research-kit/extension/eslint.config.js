12095a789e7f063812bf9b00a0ac59249e090cf3
Hoan <dodinhhoan.bkhn@gmail.com>
2026-05-17 10:59:23 +0700

    fix: eslint config - use typescript-eslint parser for TS/TSX files

--- research-kit/extension/eslint.config.js
+++ research-kit/extension/eslint.config.js
@@ -3,23 +3,29 @@ import globals from 'globals'
 import react from 'eslint-plugin-react-hooks'
 import tseslint from 'typescript-eslint'
 
-export default [
+export default tseslint.config(
   { ignores: ['dist'] },
   {
     files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+    ],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        chrome: 'readonly',
+        process: 'readonly',
+      },
     },
     plugins: {
       'react-hooks': react,
-      '@typescript-eslint': tseslint.plugin,
     },
     rules: {
-      ...js.configs.recommended.rules,
-      ...tseslint.configs.recommended.rules,
       'react-hooks/exhaustive-deps': 'warn',
       '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'off',
     },
   },
-]
+)
